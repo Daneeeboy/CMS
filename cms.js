e08523915b86ba7a14c985c5da86ead765db5aa0
Dan Hughes <dan.hughes@thebluehive.com>
2014-06-05 18:06:28 +0100

    updates

--- cms.js
+++ cms.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 
 $(document).ready(function() {
+	
+	var currentP = 0;
+	var activeP;
 		
 	function setCaret(el) {
 		var range = document.createRange();
@@ -12,27 +15,65 @@ $(document).ready(function() {
 		el.focus();
 	}
 	
-	function updateContents() {
-		$(".editable-textarea").on("keyup", function() {
-			var sel = $("#t1").getSelection();
-			$("#editable").append(sel.end);
-			$("#contents").html($(this).val());
+	function updateCanvasP(activeP) {
+		// pull the contents of the active p textarea into the corresponding display p
+		$("#p" + activeP).html($("#t" + activeP).val());
+	}
+	
+	function focusOnTextarea(currentP) {
+		// set hidden caret to relevant textarea/update currently active P.
+		$("#t" + currentP).focus();
+		activeP = currentP;	
+	}
+	
+	function createNewTextarea(currentP) {
+		// create hidden textarea and container div.  Append to row. Focus on new textarea
+		$("#r" + currentP).append('<div class="hidden_input_div"><textarea id="t' + currentP + '" class="hidden_input_textarea editable_p"></textarea>');
+		focusOnTextarea(currentP);
+		
+		// when editing hidden editable p in textarea, update the canvas P
+		$(".editable_p").on("keyup", function(e) {
+			if(e.keyCode == 13) {
+				e.preventDefault();
+				alert("hello");
+				currentP = createNewP(currentP);
+			}
+
+			updateCanvasP(currentP);	
 		});
 	}
 	
-	function createTextarea() {
-		createDisplayDiv();
-		$("#t1").focus();
-		updateContents();
+	function createNewP(currentP) {
+		// create a new paragraph (insert into db/create display div/create textarea)
+		var newP = currentP+1; // DUMMY CODE - this will be the insertion into the db that will return the correct ID.
+		$("#editable_canvas").append('<div class="canvas_row" id="r' + newP +'"><p id="p' + newP + '" class="canvas_p">&nbsp;</p></div>'); // DUMMY CODE - should be new function.  Append after current P. 
+		createNewTextarea(newP); // create textarea and hidden container div
+		
+		$(".canvas_p").on("click", function() {
+			focusOnTextarea(activeP);
+		});
+		
+		return newP;
 	}
 	
-	function createDisplayDiv() {
-		$("#editable").append('<div id="contents" class="contents"></div>');
+	function createRow(currentP) {
+		// insert new row into canvas. 
+		
+		if(currentP == "" || currentP == "0") { // dummy code.
+			currentP = 	1;
+		}
+		
+		var newP = createNewP(currentP);
+		return newP;
 	}
 	
-	$("#editable").click(function() {
-		$("#default").hide();
-		createTextarea();
+	// on click of canvas paragraph
+	$(".canvas_p").on("click", function() {
+		if($("#default").length > 0) {
+			//first click of content.  Default div exists. Remove, setup new row.
+			$("#default").remove();
+			createRow(currentP);
+		} 
 	});
 		
 });
